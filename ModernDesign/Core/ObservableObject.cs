06206e6bbc79537243f461096cad3961ae11f99d
JohnApt <70862565+valor248@users.noreply.github.com>
2022-11-19 15:25:54 -0600

    Implemented MenuButtonTheme and added Buttons.

--- ModernDesign/Core/ObservableObject.cs
+++ ModernDesign/Core/ObservableObject.cs
@@ -10,7 +10,6 @@ namespace ModernDesign.Core
 
         protected void OnPropertyChanged([CallerMemberName] string name = null)
         {
-
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
