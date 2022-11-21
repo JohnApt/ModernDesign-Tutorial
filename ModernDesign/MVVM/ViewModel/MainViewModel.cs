ca210a035e559c8f2afa486e36cf447e7af47cce
JohnApt <70862565+valor248@users.noreply.github.com>
2022-11-20 21:55:38 -0600

    Added DiscoverView and implemented context view change commands.

--- ModernDesign/MVVM/ViewModel/MainViewModel.cs
+++ ModernDesign/MVVM/ViewModel/MainViewModel.cs
@@ -9,9 +9,43 @@ namespace ModernDesign.MVVM.ViewModel
 {
 	class MainViewModel : ObservableObject
 	{
+		public RelayCommand HomeViewCommand { get; set; }
+
+		public RelayCommand DiscoverViewCommand { get; set; }
+
+
+		public HomeViewModel HomeVM { get; set; }
+
+		public DiscoverViewModel DiscoverVM { get; set; }
+
+		private object _currentView;
+
+		public object currentView
+		{
+			get { return _currentView; }
+			set
+			{
+				_currentView = value;
+				OnPropertyChanged();
+			}
+		}
+
 		public MainViewModel()
 		{
+			HomeVM = new HomeViewModel();
+			DiscoverVM = new DiscoverViewModel();
 
+			currentView = HomeVM;
+
+			HomeViewCommand = new RelayCommand(o =>
+			{
+				currentView = HomeVM;
+			});
+
+			DiscoverViewCommand = new RelayCommand(o =>
+			{
+				currentView = DiscoverVM;
+			});
 		}
 	}
 }
